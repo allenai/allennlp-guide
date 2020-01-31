1ee69ab47daf39bac0d1f8331631fa30d16c73d1
Aaron Sarnat <aaron@pixelday.com>
2020-01-30 20:38:45 -0800

    first pass functional sidenav

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,6 @@ import { StaticQuery, graphql } from 'gatsby'
 import SEO from './seo'
 import { Link } from './link'
 import { H3 } from './typography'
-import Logo from '../../static/logo.svg'
 
 import '../styles/index.sass'
 import classes from '../styles/layout.module.sass'
@@ -34,22 +33,7 @@ const Layout = ({ isHome, title, description, children }) => {
                     <>
                         <SEO title={title} description={description} />
                         <main className={classes.root}>
-                            {!isHome && (
-                                <h1 className={classes.logo}>
-                                    <Link hidden to="/">
-                                        <Logo width={150} height={54} aria-label={meta.title} />
-                                    </Link>
-                                </h1>
-                            )}
                             <div className={classes.content}>
-                                {(title || description) && (
-                                    <header className={classes.header}>
-                                        {title && <h1 className={classes.title}>{title}</h1>}
-                                        {description && (
-                                            <p className={classes.description}>{description}</p>
-                                        )}
-                                    </header>
-                                )}
                                 {children}
                             </div>
 
