44bef896d15a1bb2dba943cd795288f746bc61fc
Aaron Sarnat <aaron@pixelday.com>
2020-01-31 18:33:14 -0800

    removing unused styled

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 import SEO from './seo'
 import { Link } from './link'
