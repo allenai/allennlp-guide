7cd691cda7baa83d9d672c74bfbcb1206dd022f8
Aaron Sarnat <aaron@pixelday.com>
2020-02-05 18:23:55 -0800

    getting rid of sass and some cleanup

--- src/markdown.js
+++ src/markdown.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import rehypeReact from 'rehype-react';
 
-import Exercise from './components/exercise';
+import ExerciseComponent from './components/ExerciseComponent';
 import { TextBlockComponent } from './components/TextBlockComponent';
 import CodeBlock from './components/CodeBlock';
 import { LinkComponent } from './components/LinkComponent';
-import { H3, Hr, Ol, Ul, Li, InlineCode } from './components/typography';
 
 export const renderAst = new rehypeReact({
     createElement: React.createElement,
     components: {
-        exercise: Exercise,
+        exercise: ExerciseComponent,
         textblock: TextBlockComponent,
         codeblock: CodeBlock,
         a: LinkComponent,
-        hr: Hr,
-        h3: H3,
-        ol: Ol,
-        ul: Ul,
-        li: Li,
-        code: InlineCode,
     },
 }).Compiler;
