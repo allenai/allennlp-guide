de6c9fa1e5f32f073fefaf3779a7b34d1462ecd5
epwalsh <epwalsh10@gmail.com>
2021-04-20 19:29:10 -0700

    fix

--- exercises/part3/interpret/attacker_source.py
+++ exercises/part3/interpret/attacker_source.py
@@ -1,5 +1,8 @@
 inputs = {"sentence": "a very well-made, funny and entertaining picture."}
-archive = "https://storage.googleapis.com/allennlp-public-models/basic_stanford_sentiment_treebank-2020.06.09.tar.gz"
+archive = (
+    "https://storage.googleapis.com/allennlp-public-models/"
+    "basic_stanford_sentiment_treebank-2020.06.09.tar.gz"
+)
 predictor = Predictor.from_path(archive)
 reducer = InputReduction(predictor)  # or Hotflip(predictor)
 # if it is Hotflip, we need an extra step: reducer.initialize()
--- exercises/part3/interpret/saliency_source.py
+++ exercises/part3/interpret/saliency_source.py
@@ -1,5 +1,8 @@
 inputs = {"sentence": "a very well-made, funny and entertaining picture."}
-archive = "https://storage.googleapis.com/allennlp-public-models/basic_stanford_sentiment_treebank-2020.06.09.tar.gz"
+archive = (
+    "https://storage.googleapis.com/allennlp-public-models/"
+    "basic_stanford_sentiment_treebank-2020.06.09.tar.gz"
+)
 predictor = Predictor.from_path(archive)
 interpreter = SimpleGradient(predictor)
 interpretation = interpreter.saliency_interpret_from_json(inputs)
