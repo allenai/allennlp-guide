763f95b922813c3a1261155b64800ab79ef6b8c9
epwalsh <epwalsh10@gmail.com>
2021-08-27 14:32:49 -0700

    fix GradientDescentTrainer import

--- exercises/part1/training-and-prediction/evaluation_setup.py
+++ exercises/part1/training-and-prediction/evaluation_setup.py
@@ -20,9 +20,9 @@ from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
 from allennlp.modules.token_embedders import Embedding
 from allennlp.modules.seq2vec_encoders import BagOfEmbeddingsEncoder
 from allennlp.nn import util
+from allennlp.training import Trainer, GradientDescentTrainer
 from allennlp.training.metrics import CategoricalAccuracy
 from allennlp.training.optimizers import AdamOptimizer
-from allennlp.training.trainer import Trainer, GradientDescentTrainer
 from allennlp.training.util import evaluate
 
 
